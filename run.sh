6be14d8ef8a94b05a943cd164b9d470051c070f1
Pranav G N <pranavgn2005@gmail.com>
2026-06-04 06:38:08 +0530

    secand commit

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PROJECT_DIR="/home/pranavgn/Desktop/Air_Watch"
+export GEMINI_API_KEY="AIzaSyDrgjtsKatSF5wME1LkMKM9hPJrrBAOC_Y"
+
+cd "$PROJECT_DIR" || exit
+
+# Start servers
+python3 -m http.server 8000 &
+PID1=$!
+
+python3 "./server.py" &
+PID2=$!
+
+# Give server time to start
+sleep 1
+# Open in Chromium
+chromium-browser http://localhost:5000/ &
+
+# Kill both on Ctrl+C
+trap "echo 'Stopping...'; kill $PID1 $PID2" SIGINT
+
+wait
